fc17129ef565955e936135f7266edd97ca85d5fb
Melnikidze <46977529+Melnikidze@users.noreply.github.com>
2023-04-11 17:25:42 +0300

    added code in bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print('Hello')
+import asyncio
+from aiogram import Dispatcher, Bot
+from config_data.config import Config, load_config
+
+# async func main statement to make config and start bot
+async def main() -> None:
+    
+    # load config
+    config: Config = load_config()
+
+    # initialization bot and dispatcher
+    dp: Dispatcher = Dispatcher()
+    bot: Bot = Bot(token=config.tg_bot.token)
+
+    # missing accuumulated updates and start polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
