42be1942703a0b3adfc640f9fcfc6dbc26b1d296
Melnikidze <46977529+Melnikidze@users.noreply.github.com>
2023-04-13 11:23:37 +0300

    added registration routers in dispather

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 from aiogram import Dispatcher, Bot
 from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
 
 # async func main statement to make config and start bot
 async def main() -> None:
@@ -11,6 +12,10 @@ async def main() -> None:
     # initialization bot and dispatcher
     dp: Dispatcher = Dispatcher()
     bot: Bot = Bot(token=config.tg_bot.token)
+
+    # registration routers in dispatcher
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     # missing accuumulated updates and start polling
     await bot.delete_webhook(drop_pending_updates=True)
