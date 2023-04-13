e264cb63c6eef4cb989e7d3b96bbbbcd43fa7f95
Melnikidze <46977529+Melnikidze@users.noreply.github.com>
2023-04-13 14:07:54 +0300

    added router initialization in user_handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,13 +1,17 @@
+from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from lexicon.lexicon import LEXICON_RU
 
+# modul's level router initialization
+router: Router = Router()
+
 # handler for command "/start"
-@dp.message(CommandStart())
+@router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'])
 
 # handler for command "/help"
-@dp.message(Command(commands='help'))
+@router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])
